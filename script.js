b2065770abb53b3e6c01e1ac55e1bc8cfcb3fc90
Francesco Maxim <francesco.maxim@icloud.com>
2024-10-16 03:05:12 +0300

    some update

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ function topFunction() {
     document.body.scrollTop = 0; // Pentru Safari
     document.documentElement.scrollTop = 0; // Pentru Chrome, Firefox, IE și Opera
 }
+
+
+// var request = new XMLHttpRequest();
+
+// request.open('GET', 'https://api.ipdata.co/?api-key=a6e461d918eed3a6dba543973a4b11105af7fe95d7b7c4753e34e72a');
+
+// request.setRequestHeader('Accept', 'application/json');
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log(this.responseText);
+//   }
+// };
+
+// request.send();
