84b46689a4334d18e5e4f64360cf2a41157a6cb9
Francesco Maxim <francesco.maxim@icloud.com>
2024-10-20 19:34:45 +0300

    removed unused function

--- script.js
+++ script.js
@@ -43,24 +43,3 @@ fetch("sections.json").then(response => response.json()).then(values => values.f
             </div>`;
         projectsDiv.insertAdjacentHTML('beforeend', htmlItem);
 }));
-
-document.addEventListener("DOMContentLoaded", () => {
-
-	// Use Intersection Observer to determine if objects are within the viewport
-	const observer = new IntersectionObserver(entries => {
-	  entries.forEach(entry => {
-		if (entry.isIntersecting) {
-		  entry.target.classList.add('in-view');
-		  return;
-		}
-		entry.target.classList.remove('in-view');
-	  });
-	});
-
-	// Get all the elements with the .animate class applied
-	const allAnimatedElements = document.querySelectorAll('.animate');
-
-	// Add the observer to each of those elements
-	allAnimatedElements.forEach((element) => observer.observe(element));
-
-}); 
